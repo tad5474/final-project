76c208bbd68679e5cd3f5a07ad1dee75e8909d58
tad5474 <60856528+tad5474@users.noreply.github.com>
2020-04-13 23:13:01 -0400

    first commit

---
+++ public/scripts/script.js
@@ -0,0 +1,69 @@
+// Add id to each one of your headers so that when you hover over and then move off it changes color//
+function colorTextone()
+{
+document.querySelector("#one").style.color="black";
+document.querySelector("#one").style.fontWeight="bold";
+}
+
+function returnTextone()
+{
+document.querySelector("#one").style.color="#DB504A";
+document.querySelector("#one").style.fontWeight="normal";
+}
+function colorTexttwo()
+{
+document.querySelector("#two").style.color="black";
+document.querySelector("#two").style.fontWeight="bold";
+}
+
+function returnTexttwo()
+{
+document.querySelector("#two").style.color="#DB504A";
+document.querySelector("#two").style.fontWeight="normal";
+}
+function colorTextthree()
+{
+document.querySelector("#three").style.color="black";
+document.querySelector("#three").style.fontWeight="bold";
+}
+
+function returnTextthree()
+{
+document.querySelector("#three").style.color="#DB504A";
+document.querySelector("#three").style.fontWeight="normal";
+}
+function colorTextfour()
+{
+document.querySelector("#four").style.color="black";
+document.querySelector("#four").style.fontWeight="bold";
+}
+
+function returnTextfour()
+{
+document.querySelector("#four").style.color="#DB504A";
+document.querySelector("#four").style.fontWeight="normal";
+}
+
+function colorTextfive()
+{
+document.querySelector("#five").style.color="black";
+document.querySelector("#five").style.fontWeight="bold";
+}
+
+function returnTextfive()
+{
+document.querySelector("#five").style.color="#DB504A";
+document.querySelector("#five").style.fontWeight="normal";
+}
+
+function colorTextsix()
+{
+document.querySelector("#six").style.color="black";
+document.querySelector("#six").style.fontWeight="bold";
+}
+
+function returnTextsix()
+{
+document.querySelector("#six").style.color="#DB504A";
+document.querySelector("#six").style.fontWeight="normal";
+}
